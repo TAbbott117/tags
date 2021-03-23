26070c87a4eebaa775d93edac652ec9c641cd293
Daniel Manfield <>
2021-03-23 11:56:16 -0400

    Added Delete Route and About Page

--- views.py
+++ views.py
@@ -1,15 +1,17 @@
 from flask import request, redirect
 from datetime import datetime
 from app import app
+#this is a change
 from tag import Tag
 
 @app.route('/', methods=['GET'])
 def show_tags():
     tags = Tag.select()
-    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
+    tags_html = '\n'.join(list(map(lambda x: "<a href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
+    return "<h1>The Ultimate Tag Manager</h1><a href=\"/\">Home</a> <a href=\"/about\">About</a><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
+
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
@@ -18,3 +20,7 @@ def add_tag():
       defaults={'created_at': datetime.now(), 'updated_at': datetime.now()})
 
     return redirect('/')
+
+@app.route('/about', methods=['GET'])
+def about():
+      return '<h1>The Ultimate Tag Manager</h1><a href=\"/\">Home</a> <a href=\"/about\">About</a><h1>A Uniquely World-Changing Super-Proprietary Way of Managing Tags</h1><p>This is the only website ever designed that has at least one of the properties common to every other website ever designed.</p><p>A highly respected source has said, "I\'d rather use this site than use one of the best sites in the world!"'
